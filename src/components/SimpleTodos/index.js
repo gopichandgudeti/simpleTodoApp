cd6de7d4bff1c1b2a29fc73c14d5c45172721078
gopichandgudeti <chandgudeti@gmail.com>
2024-05-30 06:22:47 +0000

    solution

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -44,6 +44,7 @@ const initialTodosList = [
 class SimpleTodos extends Component {
   state = {
     todosList: initialTodosList,
+    input: '',
   }
 
   onDeleteTodo = id => {
@@ -52,19 +53,57 @@ class SimpleTodos extends Component {
     this.setState({todosList: filteredList})
   }
 
+  onChangeInput = event => {
+    this.setState({input: event.target.value})
+  }
+
+  onClickAdd = () => {
+    const {todosList, input} = this.state
+    const {length} = todosList
+    const newTodoItem = {
+      id: length + 1,
+      title: input,
+    }
+    this.setState(prevState => ({
+      todosList: [...prevState.todosList, newTodoItem],
+      input: '',
+    }))
+  }
+
+  onEditTodo = (id, title) => {
+    // const {todosList} = this.state
+    // const filteredListOnEdit = todosList.filter(each => each.id !== id)
+    this.setState({input: title})
+  }
+
   render() {
-    const {todosList} = this.state
+    const {todosList, input} = this.state
 
     return (
       <div className="todo-bg-container">
         <div className="todo-container">
           <h1 className="heading">Simple Todos</h1>
+          <div className="input-add-btn-container">
+            <input
+              className="input-container"
+              value={input}
+              onChange={this.onChangeInput}
+            />
+            <button
+              type="button"
+              className="add-button"
+              onClick={this.onClickAdd}
+            >
+              Add
+            </button>
+          </div>
           <ul className="ul-container">
             {todosList.map(eachItem => (
               <TodoItem
                 todoItems={eachItem}
                 key={eachItem.id}
                 deleteItem={this.onDeleteTodo}
+                editTodo={this.onEditTodo}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,21 +1,51 @@
 // Write your code here
+import React, {useState} from 'react'
 
 import './index.css'
 
 const TodoItem = props => {
-  const {todoItems, deleteItem} = props
+  const {todoItems, deleteItem, editTodo} = props
   const {id, title} = todoItems
+  const [btnStatus, setBtnStatus] = useState(true)
+  const [isChecked, setIsChecked] = useState(false)
 
   const deleteTodo = () => {
     deleteItem(id)
   }
 
+  const onClickEditSaveBtn = () => {
+    setBtnStatus(prevState => !prevState)
+    editTodo(id, title)
+  }
+
+  const onChangeCheckBox = () => {
+    setIsChecked(prevState => !prevState)
+  }
+
   return (
     <li className="list-container">
-      <p className="todoName">{title}</p>
-      <button type="button" className="delete-button" onClick={deleteTodo}>
-        Delete
-      </button>
+      <div className="checkBox-title-container">
+        <input
+          type="checkbox"
+          className="check-box"
+          onChange={onChangeCheckBox}
+        />
+        <p className={`todoName ${isChecked ? 'completed-task' : ''}`}>
+          {title}
+        </p>
+      </div>
+      <div className="buttons-container">
+        <button
+          type="button"
+          className={`edit-save-btn ${btnStatus ? '' : 'save-btn'}`}
+          onClick={onClickEditSaveBtn}
+        >
+          {btnStatus ? 'Edit' : 'Save'}
+        </button>
+        <button type="button" className="delete-button" onClick={deleteTodo}>
+          Delete
+        </button>
+      </div>
     </li>
   )
 }
